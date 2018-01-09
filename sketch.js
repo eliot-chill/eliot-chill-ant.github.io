0bdd7f375ae7cc83e2d8527047020be9163459c8
Eliot Cholerton-Hill <eliotchill99@gmail.com>
2018-01-09 16:48:58 +0000

    Trying to fix display error

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ let ANTDOWN = 2;
 let ANTLEFT = 3;
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
+  createCanvas(displayWidth, displayHeight);
   grid = make2DArray(width,height);
   x = width/2;
   y = height/2;
